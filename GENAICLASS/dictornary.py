090afc6cb653404b02aceea49f8c843f12f7a09b
Manasa Golla <golla.maansa@.gmail.com>
2026-05-28 12:44:55 -0400

    added dictionary practice script

---
+++ GENAICLASS/dictornary.py
@@ -0,0 +1,10 @@
+#dictionary using curly braces {} and they store data in key:value pairs 
+
+student = {
+    "name": "Raji",
+    "age": 25,
+    "course": "QA Automation",
+    "is_certified": True
+}
+
+print(student)
